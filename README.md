85a195df4740eb119929d6bde2b798248a5b6f58
winnytang <winny.tang@thinkpower.com.tw>
2020-03-16 18:37:05 +0800

    init

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # exercise-springboot-jboss-tomcat
 deploy a Spring Boot WAR to Wildfly, JBoss or Tomcat
+
+- [How to deploy a Spring Boot WAR to Wildfly, JBoss or Tomcat](https://thepracticaldeveloper.com/2018/08/06/how-to-deploy-a-spring-boot-war-in-wildfly-jboss/)
---
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -0,0 +1,28 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@SpringBootApplication
+public class DemoApplication extends SpringBootServletInitializer{
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(DemoApplication.class);
+	}
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoApplication.class, args);
+	}
+
+	@GetMapping("/")
+    public String home() throws Exception {
+        return "Hello,World!";
+    }
+
+}
